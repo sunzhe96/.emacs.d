4f5d55e29ab4adb46ceb985600c0e33b79da0622
Zhe Sun <sunzhe1996@gmail.com>
2020-12-08 23:01:46 +0800

    Added racket mode config

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -13,15 +13,15 @@
   (ivy-use-virtual-buffers t)
   :config (ivy-mode))
 
-(use-package ivy-rich
-  :after ivy
-  :custom
-  (ivy-virtual-abbreviate 'full
-                          ivy-rich-switch-buffer-align-virtual-buffer t
-                          ivy-rich-path-style 'abbrev)
-  :config
-  (ivy-set-display-transformer 'ivy-switch-buffer
-                               'ivy-rich-switch-buffer-transformer))
+;; (use-package ivy-rich
+;;   :after ivy
+;;   :custom
+;;   (ivy-virtual-abbreviate 'full
+;;                           ivy-rich-switch-buffer-align-virtual-buffer t
+;;                           ivy-rich-path-style 'abbrev)
+;;   :config
+;;   (ivy-set-display-transformer 'ivy-switch-buffer
+;;                                'ivy-rich-switch-buffer-transformer))
 
 (use-package swiper
   :after ivy
---
+++ lisp/init-racket.el
@@ -0,0 +1,5 @@
+(add-hook 'racket-mode-hook
+	  (lambda ()
+	    (define-key racket-mode-map (kbd "C-x C-j") 'racket-run)))
+
+(provide 'init-racket)
