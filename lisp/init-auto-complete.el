5cf8b9b62b18bf1f2bb45d2d1360f43028a25b7d
Zhe Sun <sunzhe1996@gmail.com>
2021-02-19 21:20:30 +0800

    ac only for org mode

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -1,4 +1,5 @@
 (use-package auto-complete
+  :ensure t
   :commands auto-complete-mode
   :init
   (progn
@@ -11,37 +12,7 @@
     (ac-set-trigger-key "TAB")
     (ac-config-default)
 
-    ;; (setq ac-delay 0.02)
-    ;; (setq ac-use-menu-map t)
-    ;; (setq ac-menu-height 50)
-    ;; (setq ac-use-quick-help nil) 
-    ;; (setq ac-comphist-file  "~/.emacs.d/ac-comphist.dat")
-    ;; (setq ac-ignore-case nil)
-    ;; (setq ac-dwim  t)
-    ;; (setq ac-fuzzy-enable t)
-
-    ;; (use-package ac-dabbrev
-    ;;   :config
-    ;;   (progn
-    ;;     (add-to-list 'ac-sources 'ac-source-dabbrev)))
-
-    (setq ac-modes '(js3-mode
-                     emacs-lisp-mode
-                     lisp-mode
-                     lisp-interaction-mode
-                     c-mode
-                     c++-mode
-                     go-mode
-                     python-mode
-                     js-mode
-                     js2-mode
-                     css-mode
-                     makefile-mode
-                     sh-mode
-                     xml-mode
-		     web-mode
-		     org-mode
-		     org-roam-mode
-		     html-mode))))
+    (setq ac-modes '(org-mode
+		     org-roam-mode))))
 
 (provide 'init-auto-complete)
