70be3030b18241e0d2a4b343b8658571b4e5038b
Zhe Sun <sunzhe1996@gmail.com>
2021-02-23 16:47:33 +0800

    set theme and lsp

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,8 +1,7 @@
 (use-package lsp-mode
   :init
-  :commands lsp
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         (go-mode . lsp))
+  :commands (lsp lsp-deferred)
+  :hook ((go-mode . lsp-deferred))
   :bind ("C-c l f" . 'lsp-find-definition)
 )
 
@@ -14,11 +13,16 @@
 (use-package company
   :ensure t
   :config
-  (setq company-idle-delay 0)
+  (setq company-idle-delay 0.2)
   (setq company-minimum-prefix-length 1))
 
 (use-package company-lsp
   :ensure t
   :commands company-lsp)
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
 
 (provide 'init-lsp)
